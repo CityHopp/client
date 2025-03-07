ebc63ec320c27260f9a2f21c9f8260507351477c
Leen Zien <Loli@Mac.fritz.box>
2025-03-07 12:05:33 +0100

    fix the signin component

--- src/pages/Signin.jsx
+++ src/pages/Signin.jsx
@@ -1,47 +1,62 @@
-import React from "react";
-import { useState } from "react";
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
 import axios from "axios";
-import "../pages/Signin.css";
 
 const Signin = () => {
   const [data, setData] = useState({
-    name: "",
     email: "",
     password: "",
   });
+  const [error, setError] = useState("");
+  const navigate = useNavigate();
 
   const handleChange = (e) => {
     setData({ ...data, [e.target.name]: e.target.value });
   };
 
-  const handleSubmit = (e) => {
+  const handleSubmit = async (e) => {
     e.preventDefault();
-    axios
-    .post("https://localhost:5005/signin", formData)
-    .then((response) => {
-      console.log("Signin successful:", response.data);
-    })
-    .catch((error) => {
-      console.error("Error signing in", error);
-    });
+    setError("");
 
+    try {
+      const response = await axios.post("http://localhost:5005/auth/login", data);
+      console.log("Signin successful:", response.data);
+
+      localStorage.setItem("authToken", response.data.authToken);
+      //homepage
+      navigate("/");
+    } catch (error) {
+      console.error("Error signing in", error);
+      setError("Invalid email or password. Please try again.");
+    }
   };
 
   return (
-   
     <div className="signin-container">
-      <h2>Sign In</h2>
+      <h2>Signin</h2>
+      {error && <p style={{ color: "red" }}>{error}</p>}
       <form onSubmit={handleSubmit}>
-        <label>Name:</label>
-        <input type="text" name="name" placeholder="👤 Enter your name" value={data.name} onChange={handleChange} required />
-
         <label>Email:</label>
-        <input type="email" name="email" placeholder="📧 Enter your email " value={data.email} onChange={handleChange} required />
+        <input
+          type="email"
+          name="email"
+          placeholder="📧 Enter your email"
+          value={data.email}
+          onChange={handleChange}
+          required
+        />
 
         <label>Password:</label>
-        <input type="password" name="password" placeholder="🔒 Password" value={data.password} onChange={handleChange} required />
+        <input
+          type="password"
+          name="password"
+          placeholder="🔒 Enter your password"
+          value={data.password}
+          onChange={handleChange}
+          required
+        />
 
-        <button type="submit">Sign in</button>
+        <button type="submit">Signin</button>
       </form>
     </div>
   );
